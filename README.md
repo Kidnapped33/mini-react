a6f2964c549215a71e42768eed282b7436b99ea8
Kidnapped33 <404770266@qq.com>
2024-01-13 14:20:06 +0800

    feat:textEl 和 el 使用函数创建

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 - document.createElement 可以创建元素
 - appendChild 可以添加一个子元素
 - 使用 live server 插件，不要直接右键打开浏览器
+- function(...children){return children}
+- 对象也可以使用解构...object
+
+- 单独抽离 textEl
+- textEl 和 el 使用函数创建
--- main.js
+++ main.js
@@ -1,3 +1,43 @@
+// const textEl = {
+//   type: "TEXT_ELEMENT",
+//   props: {
+//     nodeValue: "mini-react",
+//     children: [],
+//   },
+// };
+
+function createTextNode(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+const textEl = createTextNode("mini-react");
+
+// const el = {
+//   type: "div",
+//   props: {
+//     id: "app",
+//     children: [textEl],
+//   },
+// };
+
+function createEl(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children,
+    },
+  };
+}
+
+const el = createEl("div", { id: "app" }, createTextNode("mini-react"));
+
 // const dom = document.createElement("div");
 // dom.id = "app";
 // const root = document.getElementById("root");
@@ -6,22 +46,6 @@
 // const textNode = document.createTextNode("");
 // textNode.nodeValue = "mini-react";
 // dom.appendChild(textNode);
-
-const textEl = {
-  type: "TEXT_ELEMENT",
-  props: {
-    nodeValue: "mini-react",
-    children: [],
-  },
-};
-
-const el = {
-  type: "div",
-  props: {
-    id: "app",
-    children: [textEl],
-  },
-};
 
 const dom = document.createElement(el.type);
 dom.id = el.props.id;
