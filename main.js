f548575316128b7a61a26839f1e6f9229aa785a5
Kidnapped33 <404770266@qq.com>
2024-01-13 15:48:46 +0800

    feat: 实现 render 函数

--- main.js
+++ main.js
@@ -13,20 +13,41 @@ function createEl(type, props, ...children) {
     type,
     props: {
       ...props,
-      children,
+      children: children.map((child) =>
+        typeof child === "string" ? createTextNode(child) : child
+      ),
     },
   };
 }
 
-const textEl = createTextNode("mini-react");
-// const el = createEl("div", { id: "app" }, textEl);
-const el = createEl("div", { id: "app" }, createTextNode("mini-react"));
+// const textEl = createTextNode("mini-react");
+const el = createEl("div", { id: "app" }, "hello", 'mini-react');
 
-const dom = document.createElement(el.type);
-dom.id = el.props.id;
-const root = document.getElementById("root");
-root.appendChild(dom);
+// const textNode = document.createTextNode("");
+// textNode.nodeValue = textEl.props.nodeValue;
 
-const textNode = document.createTextNode(textEl.type);
-textNode.nodeValue = textEl.props.nodeValue;
-dom.appendChild(textNode);
+// const dom = document.createElement(el.type);
+// dom.id = el.props.id;
+// dom.appendChild(textNode);
+
+// const root = document.getElementById("root");
+// root.appendChild(dom);
+
+const render = (el, root) => {
+  const dom =
+    el.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(el.type);
+
+  Object.keys(el.props).forEach((key) => {
+    key !== "children" && (dom[key] = el.props[key]);
+  });
+
+  el.props.children.forEach((child) => {
+    render(child, dom);
+  });
+
+  root.appendChild(dom);
+};
+
+render(el, document.getElementById("root"));
